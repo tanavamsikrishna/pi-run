dad29b6433125584bdc703580ff49a6ecea2a8c9
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-03-21 01:17:40 +0530

    chore: remove debug.ts file

--- debug.ts
+++
@@ -1,8 +0,0 @@
-import type {
-    ExtensionAPI,
-    ExtensionContext,
-} from "@mariozechner/pi-coding-agent";
-
-export default function (pi: ExtensionAPI, getCtx: () => ExtensionContext | null) {
-    return pi.getActiveTools();
-}
